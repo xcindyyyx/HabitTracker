f8f92c452ca3c2a961f7c43b3111bd013495d3d1
Cindy Cardona <cindycardonaaa@gmail.com>
2025-06-17 14:37:30 -0400

    Error handling for menu_options and finished case 3 and case _

--- HabitTracker.py
+++ HabitTracker.py
@@ -1,8 +1,38 @@
 # This will be our main file
 
 def main():
-    # Show Menu 
+    # Show Menu
+    while True:
+        print("-- Welcome to Habit Tracker --")
+        print()
 
+        print("Menu: ")
+        print("1) Add Habit")
+        print("2) View Today's Habit")
+        print("3) Exit")
+
+        print()
+        
+        try:
+            menu_options = int(input("Enter a number 1-3: "))
+        except ValueError:
+            print("Please enter a number 1-3")
+            print()
+            continue
+
+        match menu_options:
+            case 1:
+                add_habit()
+            case 2:
+                habit_tracker()
+            case 3:
+                print("\nGoodbye!", end="")
+                break
+            case _: 
+                print("Please enter a number 1-3")
+                print()
+
+   
     # Loop Menu Options
 
     # Call MenuFunctions 
@@ -10,3 +40,5 @@ def main():
 
 
 # Do __name___ = "__main__" and call main() function (So program will be ran through this file)
+if __name__ == "__main__":
+    main()
--- MenuFunctions.py
+++ MenuFunctions.py
@@ -1,8 +1,9 @@
 # DO NOT FORGET TO PROMPT THE USER FOR QUESTIONS this is a CLI program remember.
 
 
-def users:
+def add_habit:
     # You will write all the Users necessities here
 
-def habits:
+def habit_tracker:
     # You will write all the habits necessities here
+
