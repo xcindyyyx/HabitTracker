d8c593e470cd68b057b35e5c774b4a1f5d0ec36a
Cindy Cardona <cindycardonaaa@gmail.com>
2025-06-17 22:10:18 -0400

    Made changes to the add_habit function. Not finished.

--- MenuFunctions.py
+++ MenuFunctions.py
@@ -1,21 +1,36 @@
 # DO NOT FORGET TO PROMPT THE USER FOR QUESTIONS this is a CLI program remember.
 
 
-# RERUN THIS FUNCTION AND FIX THE TRY/EXCEPT
+# RERUN THIS FUNCTION AND FIX THE TRY/EXCEPT!
 def add_habit():
 
     print('You picked "Add Habit"\n')
+    
+    while True:
+        try:
+            habit = input("Enter the habit: ")
 
-    try:
-        habit = input("Enter the habit: ")
-        habit_number = int(input("Number of times this habit will be done: "))
-    except ValueError:
-        print("Please enter a number 1-3")
-        print()
+            # Remove space and check if only letters are left
+            if habit.replace(" ", "").isalpha():
+                break       
+            else:
+                raise ValueError                 # This triggers except    
 
-    print()
-    print(f"Your habit for today {habit} and it'll be done {habit_number} times")
-    print()
+        except ValueError:
+            print("Please enter only letters and spaces. No numbers or symbols.")
+            print()
+            return                               # Exit the function
+
+    # try:
+    #     habit_number = int(input("Number of times this habit will be done: "))
+    #  except ValueError:
+    #     print("Please enter a number")
+    #     print()
+    #     return                              # Exit the function
+
+    # print()
+    # print(f"Your habit for today {habit} and it'll be done {habit_number} times")
+    # print()
     return habit, habit_number
     
 def habit_tracker():
