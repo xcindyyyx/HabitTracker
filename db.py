dd3254580faeaa5da3b0d21de2274e123534379f
Cindy Cardona <cindycardonaaa@gmail.com>
2025-06-19 15:48:28 -0400

    Created users and habits table

--- db.py
+++ db.py
@@ -1,5 +1,29 @@
-# This is where we will create two tables using SQL 
+import sqlite3
 
-# Here you will create a Users table (Primary key will be here)
+# Connect to the databse (or create it)
+conn = sqlite3.connect("habit_tracker.db")
 
-# Here you will create a Habits table (Foreign key will be here)
+# Create a cursor to interact with the DB
+cur = conn.cursor()
+
+# Create users table
+cur.execute = ("""
+    CREATE TABLE IF NOT EXISTS users(
+        user_id INTEGER UNIQUE PRIMARY KEY,
+        first_name TEXT,
+        last_name TEXT)
+""")
+
+# Create habits table (Foreign key will be here)
+cur.execute = ("""
+    CREATE TABLE IF NOT EXISTS habits(
+        habit_id INTEGER PRIMARY KEY,
+        habit_name TEXT,
+        user_id INTEGER,
+        FOREIGN KEY(user_id) REFERENCES users(user_id))
+""")
+
+# Save changes
+conn.commit()
+# Close the connection
+conn.close()
